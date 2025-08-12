9b2904f6ae9efd2e23b160b069e652acacbefbe2
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-12 18:17:16 +0500

    Update MainActivity.kt

--- android/app/src/main/java/com/example/flutter_github_full_test/MainActivity.kt
+++ android/app/src/main/java/com/example/flutter_github_full_test/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.flutter_github_full_test
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
